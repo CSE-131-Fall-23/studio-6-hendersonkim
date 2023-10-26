34fda8cac195f1268aa683a6138778acc51def2b
julianne <julianne@10.232.178.90>
2023-10-26 11:19:17 -0500

    circles
    practiced using recursions
    it was quite hard
    we drew circles and reversed and array and did geometric sums

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,9 +12,12 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
+		if(n == 0) {
+			return 0; 
+		}
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+		double sum = Math.pow(1/2,n) + geometricSum(n-1); 
+		return sum;
 		
 	}
 
@@ -44,10 +47,29 @@ public class RecursiveMethods {
 	public static int[] toReversed(int[] array) {
 		
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
+		int[] reverse = new int [array.length]; 
+		if (array.length == 0) {
+			return array; 
+		}
+		
+		return helper(array, 0, reverse);
 		
 	}
 
+	public static int[] helper(int[] array, int index, int[] reverse) { 
+		int middle = array.length/2; 
+		if(index > middle) {
+			 return reverse; 
+		}
+		else { 
+			reverse[index] = array[array.length-index-1]; 
+			reverse[array.length-index-1] = array[index]; 
+			return helper(array, index+1, reverse);
+		}
+
+	}
+	
+	
 	/**
 	 * @param xCenter                       x-coordinate of the center of the circle
 	 *                                      at the current depth
@@ -58,9 +80,22 @@ public class RecursiveMethods {
 	 * @param radiusMinimumDrawingThreshold radius above which drawing should occur
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
+			
 			double radiusMinimumDrawingThreshold) {
 		
-		// FIXME
+		if(radius >= radiusMinimumDrawingThreshold) {
+			StdDraw.circle(xCenter, yCenter, radius); 
+			double topCenter = yCenter + radius; 
+			double bottomCenter = yCenter - radius; 
+			double rightCenter = xCenter + radius; 
+			double leftCenter = xCenter - radius; 
+			double newRadius = radius/3.0; 
+			circlesUponCircles(xCenter, topCenter, newRadius, radiusMinimumDrawingThreshold); 
+			circlesUponCircles(xCenter, bottomCenter, newRadius, radiusMinimumDrawingThreshold); 
+			circlesUponCircles(rightCenter, yCenter, newRadius, radiusMinimumDrawingThreshold); 
+			circlesUponCircles(leftCenter, yCenter, newRadius, radiusMinimumDrawingThreshold); 
+		}
+		// FIXME 
 	}
 
 }
